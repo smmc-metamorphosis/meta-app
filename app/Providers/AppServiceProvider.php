3f7d99e1c22f87bd687d2e1f98dd13966120fd21
smmc-metamorphosis <smmc.metamorphosis@gmail.com>
2026-04-02 00:52:01 +0800

    Force HTTPS to fix CSS loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // This line is new
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This forces all asset() and url() links to use https 
+        // whenever the app is not running on your local computer.
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
